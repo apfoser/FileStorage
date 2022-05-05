9d94756e21ce02c7120a2809780f6d5dfa189d19
apfoser <alex.pfoser@gmail.com>
2022-05-05 18:31:49 -0400

    import errors

--- fileSplitting.py
+++ fileSplitting.py
@@ -1,8 +1,5 @@
 import hashlib
 import sys
-
-from pandas import merge
-
 '''
 This program takes in a .txt file and splits it into 64kb chunks. Stores the hashes of the chunks in a Dic.
 hash --> md5
@@ -20,7 +17,7 @@ BUF_SIZE = 256
 chunks = {}
 storage = {}
 
-fileSplit = file()
+
 
 '''
 Takes the system arguments and splits them into 32 byte chunks. 
@@ -29,6 +26,7 @@ Hashes chunks and points to address of host
 def split(filename):
     with open(filename, 'rb') as f:
         counter = 0
+        fileSplit = file()
         while True:
             data = f.read(BUF_SIZE)
             if not data:
@@ -39,6 +37,8 @@ def split(filename):
                 storage[hashed] = data
             fileSplit.order[counter] = hashed
             counter += 1
+            
+    return fileSplit
              
             
         
@@ -48,9 +48,7 @@ Pieces together file, for now ignores address of host (not implemented)
 File object needs to be implemented
 '''
 def retrieve(file):
-    
     merged = b''
-    
     for i in range(len(file.order)):
         merged += storage[file.order[i]]
 
@@ -64,11 +62,14 @@ def retrieve(file):
 split(sys.argv[1])
 split(sys.argv[2])
 
-print(fileSplit.order)
+fileA = split(sys.argv[1])
+fileB = split(sys.argv[2])
+
 print(chunks)
 
-retrieve(fileSplit)
-
+retrieve(fileA)
+print('/n')
+retrieve(fileB)
     
     
         
