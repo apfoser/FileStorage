d6687dfb8c9314b1bfc5e30e6787d4756f8ec853
apfoser <alex.pfoser@gmail.com>
2022-05-17 18:20:52 -0400

    mac fix

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ port = 5001
 print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected to ", host)
-filename = input("File to Transfer : ")
+filename = input("File to Transfer: ")
 filesize = os.path.getsize(filename)
 s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 #file = open(filename, 'wb') 
